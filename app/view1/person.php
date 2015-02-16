a22044f75be75842757f1c4bb246a0083f8c6679
emanny70 <gitH4proj>
2015-02-16 15:42:52 -0800

    added PHP file to connect to the database. got one user from the database.

---
+++ app/view1/person.php
@@ -0,0 +1,29 @@
+<?php
+//Variables for connecting to your database.
+//These variable values come from your hosting account.
+$hostname = "forgottenGenocid.db.5079411.hostedresource.com";
+$username = "forgottenGenocid";
+$dbname = "forgottenGenocid";
+
+//These variable values need to be changed by you before deploying
+$password = "my1stDB2!";
+$usertable = "Person";
+$yourfield = "name";
+
+//Connecting to your database
+mysql_connect($hostname, $username, $password) OR DIE ("Unable to
+connect to database! Please try again later.");
+mysql_select_db($dbname);
+
+//Fetching from your database table.
+$query = "SELECT * FROM $usertable";
+$result = mysql_query($query);
+
+if ($result) {
+    while($row = mysql_fetch_array($result)) {
+        $name = $row["$yourfield"];
+        echo "$name";
+    }
+}
+?>
+        
