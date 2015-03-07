11a9fd87f2428e6ef8ba5d4f6d53010ccecdb251
manny <eohannessian@entertainmentpartners.com>
2015-03-06 16:51:05 -0800

    added login

---
+++ app/login/api/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'forgottenGenocid');
+define('DB_PASSWORD', 'my1stDB2!');
+define('DB_HOST', 'forgottenGenocid.db.5079411.hostedresource.com');
+define('DB_NAME', 'forgottenGenocid');
+
+?>
