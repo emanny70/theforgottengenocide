6e1caa9e102a15d0576aa0694e11255b6ce51de3
emanny70 <emanny70@gmail.com>
2015-03-17 22:29:38 -0700

    made login project the main page

--- app/login/index.php
+++ app/login/index.php
@@ -8,8 +8,9 @@
           <title>AngularJS Authentication App</title>
           <!-- Bootstrap -->
           <link href="css/bootstrap.min.css" rel="stylesheet">
-            <link href="css/custom.css" rel="stylesheet">
-              <link href="css/toaster.css" rel="stylesheet">
+          <link href="css/custom.css" rel="stylesheet">
+          <link href="css/toaster.css" rel="stylesheet">
+          <link href="css/the-forgotten-genocide.css" rel="stylesheet">
                 <style>
                   a {
                   color: orange;
@@ -25,27 +26,6 @@
               </head>
 
   <body ng-cloak="">
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="row">
-          <div class="navbar-header col-md-8">
-            <button type="button" class="navbar-toggle" toggle="collapse" target=".navbar-ex1-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" rel="home" title="AngularJS Authentication App">AngularJS Authentication App</a>
-          </div>
-          <div class="navbar-header col-md-2">
-            <a class="navbar-brand" rel="home" title="AngularJS Authentication Tutorial" href="http://www.angularcode.com/user-authentication-using-angularjs-php-mysql">Tutorial</a>
-          </div>
-           <div class="navbar-header col-md-2">
-            <a class="navbar-brand" rel="home" title="Download" href="https://app.box.com/s/1uvn9xo9nbi4xxm9g9dx">Download</a>
-          </div>
-        </div>
-      </div>
-    </div>
     <div >
       <div class="container" style="margin-top:20px;">
 
